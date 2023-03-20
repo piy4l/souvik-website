b3f0f60b531a2cb415e168aca6f1a12cba77aead
piy4l <1705034@ugrad.cse.buet.ac.bd>
2023-03-20 11:31:11 +0600

    slight update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Souvik Das</h1>
-        <h5 className="text-light">CS Graduate | CSCA™</h5>
+        <h5 className="text-light">CS Undergrad | CSCA™</h5>
         
         <CTA />
         <HeaderSocials />
