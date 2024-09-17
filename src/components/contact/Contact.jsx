6e645cce7328efb032618deb862c30d0096fe142
Souvik Das <souvik.piyal@gmail.com>
2024-09-18 02:00:00 +0600

    updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+8801760449363</h5>
-            <a href="https://api.whatsap.com/send?phone=+8801760449363" target='_blank'>Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=+8801760449363" target='_blank'>Send a message</a>
           </article>
         </div>
 
